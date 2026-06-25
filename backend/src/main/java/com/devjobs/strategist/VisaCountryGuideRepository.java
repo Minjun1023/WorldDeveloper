6d035bd05e3862369d86f7146462229f4edc567b
minjunsong <songmj5000@naver.com>
2026-06-25 19:14:59 +0900

    feat(visa-guide): VisaCountryGuide 엔티티 + 리포지토리

---
+++ backend/src/main/java/com/devjobs/strategist/VisaCountryGuideRepository.java
@@ -0,0 +1,5 @@
+package com.devjobs.strategist;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VisaCountryGuideRepository extends JpaRepository<VisaCountryGuide, String> {}
